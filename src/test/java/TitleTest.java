ea6fcd549aff26bd3ba23ea96e8b48b8d7c45a02
Rupesh <rupeshgiri30@gmail.com>
2025-09-08 23:01:16 -0400

    CICD Implement

---
+++ src/test/java/TitleTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class TitleTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.edge.driver", "C:\\driver\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void testGoogleHomePage() {
+        driver.get("https://www.google.com");
+        String pageTitle =  driver.getTitle();
+        System.out.println("Page title is: " + driver.getTitle());
+        Assert.assertEquals(pageTitle, "Google", "Title does not match!");
+    }
+
+}
